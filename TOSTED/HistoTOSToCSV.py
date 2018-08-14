423ecb17868e23667997dd44e5a256dfb2313d45
Work PTLC <cogito.ergo.p@gmail.com>
2018-08-14 16:05:57 +0200

    Histo TOS to CSV

---
+++ TOSTED/HistoTOSToCSV.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import sys
+import csv
+from collections import defaultdict
+
+def HistoTOSToCSV(inputCSV, outputCSV):
+    # Indexation de la table des noms logiques
+    nomLogiqueIndex = defaultdict(lambda: defaultdict(str))
+    with open("TableNomLogiqueHistoTOS.csv", "rb") as nomLogiqueTableFile:
+        nomLogiqueTableCSVReader = csv.reader(nomLogiqueTableFile, delimiter=';')
+        nomLogiqueTableCSVReader.next() # Ignore la premiere ligne
+        for row in nomLogiqueTableCSVReader:
+            nomLogique = unicode(row[0], "utf-8")
+            libelle = unicode(row[1], "utf-8")
+            mnemonique = unicode(row[5], "utf-8")
+            nomLogiqueIndex[mnemonique][libelle] = nomLogique
+
+    # Indexation de la table des codes
+    codeIndex = defaultdict(lambda: defaultdict(str))
+    with open("TableCodeHistoTOS.csv", "rb") as codeTableFile:
+        codeTableCSVReader = csv.reader(codeTableFile, delimiter=';')
+        codeTableCSVReader.next() # Ignore la premiere ligne
+        for row in codeTableCSVReader:
+            mnemonique = unicode(row[0], "utf-8")
+            libelle1 = unicode(row[4], "utf-8")
+            code = unicode(row[2], "utf-8")
+            codeIndex[mnemonique][libelle1] = code
+
+    # Formatage
+    inputCSVFile = open(inputCSV, "rb")
+    outputCSVFile = open(outputCSV, "wb")
+    inputCSVReader = csv.reader(inputCSVFile, delimiter=';')
+    outputCSVWriter = csv.writer(outputCSVFile, delimiter=';')
+
+    inputCSVReader.next() # Ignore la premiere ligne
+    for row in inputCSVReader:
+        evenement = row[7]
+
+        # Ne pas traiter la ligne si il ne s'agit pas d'un debut ou d'une fin d'alarme
+        if evenement == "Mise en attente d'une IDP":
+            valeur = 1
+        elif evenement == "Fin d'une IDP":
+            valeur = 0
+        else:
+            continue
+
+        horodatage = unicode(row[0][:-2], "ISO-8859-1")
+        typeEqpt = unicode(row[2], "ISO-8859-1")
+        libelleEqpt = unicode(row[3], "ISO-8859-1")
+        libelleAlarme = unicode(row[6], "ISO-8859-1")
+        nomLogique = nomLogiqueIndex[typeEqpt][libelleEqpt]
+        code = codeIndex[typeEqpt][libelleAlarme]
+        outputCSVWriter.writerow((nomLogique, code, valeur, horodatage))
+
+    inputCSVFile.close()
+    outputCSVFile.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: {} [INPUT_CSV] [OUTPUT_CSV]".format(__file__))
+        sys.exit(0)
+        
+    inputCSV = sys.argv[1]
+    outputCSV = sys.argv[2]
+
+    HistoTOSToCSV(inputCSV, outputCSV)
